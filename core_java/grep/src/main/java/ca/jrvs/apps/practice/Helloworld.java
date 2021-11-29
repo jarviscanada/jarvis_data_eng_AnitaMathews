c572b9321cce38ebe756db0f99df036338d5e419
Anita Mathews <anita.rachel.mathews@gmail.com>
2021-11-29 16:21:33 +0000

    pom.xml and regex practice

---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/Helloworld.java
@@ -0,0 +1,8 @@
+package ca.jrvs.apps.practice;
+
+class HelloWorld {
+    //Prints hello world to the terminal
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
