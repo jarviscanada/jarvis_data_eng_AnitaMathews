cf5dbf8c14973d0edf3288d300c535e183d64279
Anita Mathews <anita.rachel.mathews@gmail.com>
2021-12-07 17:39:33 +0000

    updating customer

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCexecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCexecutor.java
@@ -11,28 +11,12 @@ public class JDBCexecutor {
             try {
                 Connection connection = dcm.getConnection();
                 CustomerDAO customerDAO = new CustomerDAO(connection);
-                Customer customer = customerDAO.findById(1000);
-                System.out.println(customer.getFirstName() + " " + customer.getLastName());
-                /*
-                CustomerDAO customerDAO = new CustomerDAO(connection);
-                Customer customer = new Customer();
-                customer.setFirstName("George");
-                customer.setLastName("Washington");
-                customer.setEmail("george.washington@wh.gov");
-                customer.setPhone("(555) 123-4567");
-                customer.setAddress("1234 Main st");
-                customer.setCity("Mount Vernon");
-                customer.setState("VA");
-                customer.setZipCode("22121");
+                Customer customer = customerDAO.findById(10000);
+                System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " + customer.getEmail());
+                customer.setEmail("gwashington@wh.gov");
+                customer = customerDAO.update(customer);
+                System.out.println(customer.getFirstName() + " " + customer.getLastName() + " " + customer.getEmail());
 
-                customerDAO.create(customer);
-
-                 */
-                //        Statement statement = connection.createStatement();
-                //        ResultSet resultSet = statement.executeQuery("SELECT COUNT(*) FROM CUSTOMER");
-                //        while (resultSet.next()) {
-                //            System.out.println(resultSet.getInt(1));
-                // }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
